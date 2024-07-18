6348fd65a27fbb624a557cbf3be48f89fc5af82e
ksard <flastdtb@yandex.ru>
2024-07-18 20:27:49 +0500

    update

--- main.py
+++ main.py
@@ -17,16 +17,19 @@ def read_file(filename):
         f_r=DictReader(data)
         return list(f_r)
 
+def standart_write(filename,res):
+    with open(filename,'w',encoding='utf-8') as data:
+        f_w=DictWriter(data,fieldnames=['Имя','Фамилия','Телефон'])
+        f_w.writeheader()
+        f_w.writerows(res)
+
 def write_file(filename,lst):
     res=read_file(filename)
     obj={'Имя':lst[0],
          'Фамилия':lst[1],
          'Телефон':lst[2]}
     res.append(obj)
-    with open(filename,'w',encoding='utf-8') as data:
-        f_w=DictWriter(data,fieldnames=['Имя','Фамилия','Телефон'])
-        f_w.writeheader()
-        f_w.writerows(res)
+    standart_write(filename,res)
 
 def row_search(filename):
     last_name=input('Введите Фамилию:')    
@@ -35,7 +38,22 @@ def row_search(filename):
         if last_name==row['Фамилия']:
             return row
     return "row not found"
-    
+
+def row_delete(filename):
+    number=int(input('Введите номер строки:'))    
+    res=read_file(filename)
+    res.pop(number-1)
+    standart_write(filename,res)
+
+def update_row(filename):
+    number=int(input('Введите номер строки:'))    
+    res=read_file(filename)
+    lst=get_date()
+    res[number-1]={'Имя':lst[0],
+         'Фамилия':lst[1],
+         'Телефон':lst[2]}
+    standart_write(filename,res)
+
 filename='phone.csv'
 
 def main():
@@ -57,5 +75,16 @@ def main():
                 print('File not found create it')
                 continue
             print(row_search(filename))
+        elif command == 'd':
+            if not exists(filename):
+                print('File not found create it')
+                continue
+            print(row_delete(filename))
+        elif command == 'u':
+            if not exists(filename):
+                print('File not found create it')
+                continue
+            update_row(filename)
+            print(read_file(filename))
             
 main()
